8892d07a488e7b49960d5369d57ae470eb90d779
jhuang60 <jhuang60@hawk.iit.edu>
2019-01-20 16:01:59 -0600

    Chapter 2 exercises completed

--- calc.rb
+++ calc.rb
@@ -1,1 +1,28 @@
+# Chapter 2 numbers
 puts 1+2
+puts 3
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+#Spaces don't matter
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# More complex expressions
+puts 5 * ( 12 - 8 ) + -15
+puts 98 + (59872 / (13 * 8)) * -51
+
+# Hours in a year
+puts 24 * 365
+# minutes in a decade
+puts ((365 * 24) * 60) * 10
+#Age in seconds
+puts (((365 * 24) * 60) * 60) * 21
+# Author's age in seconds to years
+puts (((1160000000 / 60) /60 ) /24) / 365
