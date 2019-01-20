396fb42a0133f149c58a8b6f22856dcf90ff91a8
jhuang60 <jhuang60@hawk.iit.edu>
2019-01-20 16:22:32 -0600

    Chapter 3 completed

---
+++ letters.rb
@@ -0,0 +1,29 @@
+# Chapter 3 letters
+
+puts 'Hello World'
+puts ''
+puts 'Good-bye.'
+
+# String Arithmetics
+puts 'I like ' + 'apple pie.'
+puts 'blink' * 4
+
+# 12 vs '12'
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# Problems
+#puts '12' + 12
+#puts '2' * '5'
+puts 'pig' * 5 # 5 sets of string pig
+#puts 5 * 'pig' pig sets of number 5
+
+puts 'you\'re swell'
+puts 'backslash at the end of the string: \\'
+puts 'up\\down'
+puts 'up\down'
